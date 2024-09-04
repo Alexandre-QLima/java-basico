2e26b8546f71d4c966733385684853c398028c70
Alexandre-QLima <alexandre493@outlook.com>
2024-09-04 20:41:10 -0300

    tipos-variaveis

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,29 @@
+public class Operadores {
+    public static void main(String[] args) {
+        
+    String contatenacao ="?";
+
+    contatenacao = 1+1+1+"1";
+
+    System.out.println(contatenacao);
+    
+    contatenacao = 1+"1"+1+1;
+
+    System.out.println(contatenacao);
+    
+    contatenacao = 1+"1"+1+"1";
+
+    System.out.println(contatenacao);
+    
+    contatenacao = "1"+1+1+1;
+
+    System.out.println(contatenacao);
+    
+    contatenacao = "1"+(1+1+1);
+
+    System.out.println(contatenacao);
+
+    
+
+    }
+}
